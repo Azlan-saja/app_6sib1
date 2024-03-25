d3aed6ab327816cc92863c80f718856ddcb445f7
Azlan-saja <azlansaja19@gmail.com>
2024-03-25 14:03:30 +0700

    Review UTS

--- lib/login.dart
+++ lib/login.dart
@@ -2,58 +2,110 @@ import 'package:app_6sib1/dashboard.dart';
 import 'package:app_6sib1/widget/my_widget.dart';
 import 'package:flutter/material.dart';
 
-class MyLogin extends StatelessWidget {
+class MyLogin extends StatefulWidget {
   const MyLogin({super.key});
+
+  @override
+  State<MyLogin> createState() => _MyLoginState();
+}
+
+class _MyLoginState extends State<MyLogin> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(14),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const MyTextSedang(text: 'Hey,'),
-              const MyTextBesar(text: 'Login Now!'),
-              const MyTextKecil(text: 'Jangan Lupa Tersenyum, vroh.'),
-              const SizedBox(
-                height: 25,
-              ),
-              const MyTextFormField(
-                maxLength: 35,
-                keyboardType: TextInputType.emailAddress,
-                icon: Icons.email,
-                labelText: 'Email',
-                helperText: 'Email Harus Aktif',
-              ),
-              const MyTextFormField(
-                maxLength: 8,
-                keyboardType: TextInputType.visiblePassword,
-                icon: Icons.lock,
-                labelText: 'Password',
-                helperText: 'Password Harus Gabungan Angka, Huruf dan Karakter',
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  MyElevatedButtonSubmit(
-                      onPressed: () {
-                        Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const MyDashboard(),
-                            ));
-                      },
-                      text: 'Login Now'),
-                  MyElevatedButtonCancel(onPressed: () {}, text: 'Ulangi'),
-                ],
-              ),
-            ],
+        child: Form(
+          key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.all(14),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const MyTextSedang(text: 'Hey,'),
+                const MyTextBesar(text: 'Login Now!'),
+                const MyTextKecil(text: 'Jangan Lupa Tersenyum, vroh.'),
+                const SizedBox(
+                  height: 25,
+                ),
+                MyTextFormField(
+                    controller: email,
+                    maxLength: 35,
+                    keyboardType: TextInputType.emailAddress,
+                    icon: Icons.email,
+                    labelText: 'Email',
+                    helperText: 'Email Harus Aktif',
+                    validator: (p0) {
+                      return (p0 == null || p0.isEmpty)
+                          ? 'Email Tidak Boleh Kosong!'
+                          : null;
+                    }),
+                MyTextFormField(
+                    controller: password,
+                    maxLength: 8,
+                    keyboardType: TextInputType.visiblePassword,
+                    icon: Icons.lock,
+                    labelText: 'Password',
+                    helperText:
+                        'Password Harus Gabungan Angka, Huruf dan Karakter',
+                    validator: (p0) {
+                      return (p0 == null || p0.isEmpty)
+                          ? 'Password Tidak Boleh Kosong!'
+                          : null;
+                    }),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    MyElevatedButtonSubmit(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            if (email.text == 'azlansaja19@gmail.com' &&
+                                password.text == '12345678') {
+                              // Login Berhasil
+                              Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => const MyDashboard(),
+                                  ));
+                            } else {
+                              // Login Gagal
+                              return showDialog<void>(
+                                context: context,
+                                builder: (BuildContext context) => AlertDialog(
+                                  title: const MyTextBesar(text: 'Informasi'),
+                                  content: const MyTextKecil(
+                                      text: 'Email dan Password Salah!'),
+                                  actions: <TextButton>[
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                      child: const Text('Close'),
+                                    )
+                                  ],
+                                ),
+                              );
+                            }
+                          }
+                        },
+                        text: 'Login Now'),
+                    MyElevatedButtonCancel(
+                        onPressed: () {
+                          email.clear();
+                          password.clear();
+                        },
+                        text: 'Ulangi'),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/pages/volume.dart
+++ lib/pages/volume.dart
@@ -10,23 +10,97 @@ class MyVolume extends StatefulWidget {
 
 class _MyVolumeState extends State<MyVolume> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-
-  TextEditingController nilaiPanjang = TextEditingController();
-  TextEditingController nilaiLebar = TextEditingController();
-  TextEditingController hasilLuas = TextEditingController();
-
-  @override
-  void dispose() {
-    super.dispose();
-    nilaiPanjang.clear();
-    nilaiLebar.clear();
-    hasilLuas.clear();
-  }
+  final TextEditingController nilaiJarijari = TextEditingController();
+  final TextEditingController nilaiTinggi = TextEditingController();
+  final TextEditingController hasilVolume = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-     return const Center(
-      child: MyTextBesar(text: 'Layer Volume'),
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const MyTextBesar(text: 'Hitung Volume'),
+            const MyTextBesar(text: 'Tabung'),
+            const MyTextSedang(text: 'Rumus π x r² x t'),
+            MyTextFormField(
+              maxLength: 10,
+              keyboardType: TextInputType.number,
+              icon: Icons.input,
+              labelText: 'Nilai Jari-jari',
+              helperText: 'Silahkan Input Nilai Jari-jari',
+              controller: nilaiJarijari,
+              validator: (p0) {
+                return (p0 == null || p0.isEmpty)
+                    ? 'Nilai Jari-jari Tidak Boleh Kosong!'
+                    : null;
+              },
+            ),
+            MyTextFormField(
+              maxLength: 10,
+              keyboardType: TextInputType.number,
+              icon: Icons.input,
+              labelText: 'Nilai Tinggi',
+              helperText: 'Silahkan Input Nilai Tinggi',
+              controller: nilaiTinggi,
+              validator: (p0) {
+                return (p0 == null || p0.isEmpty)
+                    ? 'Nilai Tinggi Tidak Boleh Kosong!'
+                    : null;
+              },
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                MyElevatedButtonSubmit(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        double result = 0;
+                        result = 3.14 *
+                            (double.parse(nilaiJarijari.text) *
+                                double.parse(nilaiJarijari.text)) *
+                            double.parse(nilaiTinggi.text);
+                        hasilVolume.text = result.toString();
+
+                        return showDialog<void>(
+                          context: context,
+                          builder: (BuildContext context) => AlertDialog(
+                            title: const MyTextBesar(text: 'Hasil'),
+                            content: MyTextKecil(
+                                text:
+                                    'π x r² x t = Volume\n\n 3.14 x ( ${nilaiJarijari.text} x ${nilaiJarijari.text} ) x ${nilaiTinggi.text} = ${hasilVolume.text}'),
+                            actions: <TextButton>[
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: const Text('Close'),
+                              )
+                            ],
+                          ),
+                        );
+                      }
+                      // print(nilaiPanjang * nilaiLebar);
+                    },
+                    text: 'Hitung'),
+                MyElevatedButtonCancel(
+                    onPressed: () {
+                      nilaiJarijari.clear();
+                      nilaiTinggi.clear();
+                      hasilVolume.clear();
+                    },
+                    text: 'Ulangi'),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
